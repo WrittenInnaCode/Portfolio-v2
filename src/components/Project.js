191caded6fa7fa2a19de0619312415b6330132a5
WrittenInnaCode <writteninnacode@gmail.com>
2022-11-28 04:27:46 -0800

    added @media to portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,25 +5,46 @@ function Project({ project }) {
     const { name, description, deployedLink, githubLink, image } = project;
 
     return (
+        <div>
+            <div className='media-portfolio-1'>
+                <div className="portfolio-box" key={name}>
+                    <img
+                        src={require(`../assets/images/portfolio/${image}`)}
+                        alt={name}
+                        className="img-fluid"
+                    />
+                    <div className="portfolio-caps">
+                        <div className="portfolio-content gradient-text text-center">
 
-        <div className="portfolio-box" key={name}>
-            <img
-                src={require(`../assets/images/portfolio/${image}`)}
-                alt={name}
-                className="img-fluid"
-            />
-            <div className="portfolio-caps">
-                <div className="portfolio-content gradient-text">
+                            <a href={deployedLink} target="_blank" rel="noreferrer">{name}</a>
+                            <a href={githubLink} target="_blank" rel="noreferrer"><i className='fab fa-github'></i></a>
+                            <p className="fs-6">{description}</p>
 
-                    <a href={deployedLink} target="_blank" rel="noreferrer">{name}</a> 
-                    <a href={githubLink} target="_blank" rel="noreferrer"><i className='fab fa-github'></i></a>
-                    <p className="fs-6">{description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div className='media-portfolio-2'>
+                <div className="portfolio-box-media text-center bg-light bg-opacity-50 shadow p-1" key={name}>
+                <a href={deployedLink} target="_blank" rel="noreferrer"><img
+                        src={require(`../assets/images/portfolio/${image}`)}
+                        alt={name}
+                        className="img-fluid"
+                    /></a>
+                    <div className="portfolio-caps-media">
+                        <div className="portfolio-content-media gradient-text text-center pt-2">
+
+                            <a href={deployedLink} target="_blank" rel="noreferrer">{name}</a> <br/>
+                            <a href={githubLink} target="_blank" rel="noreferrer"><i className='fab fa-github'></i> Repo</a>
+                            <p className="fs-6">{description}</p>
+
+                        </div>
+                    </div>
                 </div>
             </div>
 
         </div>
-
     )
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,14 +6,14 @@ function Portfolio() {
     const [projects] = useState([
         {
             name: "Courier App",
-            description: "Javascript/Express/Sequelize/Handlebars",
+            description: "Javascript, Express, Sequelize, Handlebars",
             deployedLink: "https://courier-share.herokuapp.com/info",
             githubLink: "https://github.com/joejhansen/courier-app",
             image: "14-courier.png"
         },
         {
             name: "Tech Blog",
-            description: "Node/MySQL/Sequalize",
+            description: "Node, MySQL, Sequalize",
             deployedLink: "https://writteninnacode-techblog.herokuapp.com",
             githubLink: "https://github.com/WrittenInnaCode/Tech_Blog",
             image: "11-techblog.png"
@@ -27,63 +27,63 @@ function Portfolio() {
         },
         {
             name: "Just Another Text Editor",
-            description: "Javascript/HTML/CSS",
+            description: "Javascript, HTML, CSS",
             deployedLink: "https://writteninnacode-text-editor.herokuapp.com",
             githubLink: "https://github.com/WrittenInnaCode/PWA-Text-Editor",
             image: "13-jate.png"
         },
         {
             name: "Weather Dashboard",
-            description: "Javascript/API/Bootstrap",
+            description: "Javascript, API, Bootstrap",
             deployedLink: "https://writteninnacode.github.io/Weather_Dashboard/",
             githubLink: "https://github.com/WrittenInnaCode/Weather_Dashboard",
             image: "04-weather-dashboard.png"
         },
         {
             name: "Team Generator",
-            description: "Javascript/NodeJS/HTML",
+            description: "Javascript, NodeJS, HTML",
             deployedLink: "https://drive.google.com/file/d/1cH1TXfoxr3lshCsMHWP3k2lzU_CL-cNZ/view",
             githubLink: "https://github.com/WrittenInnaCode/Team_Generator",
             image: "07-team-generator.png"
         },
         {
             name: "Code Quiz",
-            description: "Javascript/jQuery/CSS",
+            description: "Javascript, jQuery, CSS",
             deployedLink: "https://writteninnacode.github.io/quiz_challenge/",
             githubLink: "https://github.com/WrittenInnaCode/quiz_challenge",
             image: "02-quiz.png"
         },
         {
             name: "Note Taker",
-            description: "Javascript/Express/NodeJS",
+            description: "Javascript, Express, NodeJS",
             deployedLink: "https://coherent-silken-peripheral.glitch.me",
             githubLink: "https://github.com/WrittenInnaCode/Note_Taker",
             image: "08-notetaker.png"
         },
         {
             name: "Social Network API",
-            description: "Javascript/",
+            description: "Javascript, ",
             deployedLink: "https://coherent-silken-peripheral.glitch.me",
             githubLink: "https://github.com/WrittenInnaCode/Note_Taker",
             image: "12-social-network.png"
         },
         {
             name: "E-commerce Back-End",
-            description: "Javascript/",
+            description: "Javascript, ",
             deployedLink: "https://drive.google.com/file/d/1NTfci7-Cof0lJRJ41d06fABBXzNGwxFC/view",
             githubLink: "https://github.com/WrittenInnaCode/E-commerce_Back_End",
             image: "10-ecommerce.png"
         },
         {
             name: "Employee Tracker",
-            description: "Javascript/",
+            description: "Javascript, ",
             deployedLink: "https://drive.google.com/file/d/1vr-zqlxnBwrkt8yWQ14ceHuF048aZcuH/view",
             githubLink: "https://github.com/WrittenInnaCode/Employee_Tracker",
             image: "09-employee-tracker.png"
         },
         {
             name: "Workday Scheduler",
-            description: "Javascript/",
+            description: "Javascript, ",
             deployedLink: "https://writteninnacode.github.io/Work_Day_Scheduler/",
             githubLink: "https://github.com/WrittenInnaCode/Work_Day_Scheduler",
             image: "03-workday-scheduler.png"
