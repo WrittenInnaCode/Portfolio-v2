b2a9c83aeecef80a0c3e1574f1efcd970929c03a
WrittenInnaCode <writteninnacode@gmail.com>
2023-08-24 16:12:54 -0700

    added emailjs functionality to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,63 @@
-import React, { useState } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
-import { validateEmail } from "../../utils/helpers"
+import React, { useRef, useState } from 'react';
+import { Form, Button, Row, Col, Alert } from 'react-bootstrap';
+// import { validateEmail } from "../../utils/helpers"
+import emailjs from '@emailjs/browser';
 
 function Contact() {
 
-    const [errorMessage, setErrorMessage] = useState("");
+    // const [errorMessage, setErrorMessage] = useState("");
 
-    const handleChange = (e) => {
-        if (e.target.name === "email") {
-            const valid = validateEmail(e.target.value);
-            if (!valid) {
-                setErrorMessage("Please enter a valid email");
-            } else {
-                setErrorMessage("");
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`Your ${e.target.name} cannot be left blank`);
-            } else {
-                setErrorMessage("");
-            }
-        }
-    };
+    // const handleChange = (e) => {
+    //     if (e.target.name === "email") {
+    //         const valid = validateEmail(e.target.value);
+    //         if (!valid) {
+    //             setErrorMessage("Please enter a valid email");
+    //         } else {
+    //             setErrorMessage("");
+    //         }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`Your ${e.target.name} cannot be left blank`);
+    //         } else {
+    //             setErrorMessage("");
+    //         }
+    //     }
+    // };
+
+
+    const [formState, setFormState] = useState({
+        user_name: "",
+        user_email: "",
+        message: "",
+    });
+
+    const [hiddenState, setHiddenState] = useState(true);
+
+    const { user_name, user_email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_a450rd2', 'template_yp6q3cw', form.current, 'NgPjSMCh3eb4HB0WL')
+            .then((result) => {
+                console.log(result.text);
+
+                setFormState({ user_name: "", user_email: "", message: "" })
+                setHiddenState(false);
+
+            })
+            .catch((error) => {
+                console.log(error.text);
+            });
+
+        setHiddenState(true);
+    }
 
 
     return (
@@ -32,42 +68,64 @@ function Contact() {
             <div className='contactMe pt-3'>
                 <Row className='bg-light bg-opacity-50 rounded-4 p-5 shadow'>
                     <Col sm={8}>
-                        <Form>
+                        <Form ref={form} onSubmit={sendEmail} className="contact-form">
 
                             <Form.Group className="mb-3">
                                 <Form.Label>Your Name</Form.Label>
                                 <Form.Control
+                                    required
                                     type="name"
-                                    name="name"
+                                    value={user_name}
+                                    name="user_name"
                                     placeholder="Full Name"
-                                    onBlur={handleChange} />
+                                    className="name-input"
+                                    onChange={handleChange}
+                                // name="name"
+                                // onBlur={handleChange} 
+                                />
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label>Email address</Form.Label>
                                 <Form.Control
+                                    required
                                     type="email"
-                                    name="email"
+                                    value={user_email}
+                                    name="user_email"
                                     placeholder="name@example.com"
-                                    onBlur={handleChange} />
+                                    className="email-input"
+                                    onChange={handleChange}
+                                // name="email"
+                                // onBlur={handleChange} 
+                                />
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label>Your Message</Form.Label>
                                 <Form.Control
+                                    required
                                     as="textarea"
                                     name="message"
+                                    value={message}
                                     placeholder="Type your message here" rows={3}
-                                    onBlur={handleChange} />
+                                    onChange={handleChange}
+                                // onBlur={handleChange} 
+                                />
                             </Form.Group>
 
-                            {errorMessage && (
+                            {/* {errorMessage && (
                                 <div>
                                     <p className='gradient-text fs-6'>{errorMessage}</p>
                                 </div>
-                            )}
+                            )} */}
 
-                            <Button variant="light">Send</Button>
+                            <Button variant="light" type="submit" value="send" id="send-email">Send</Button>
+
+                            <div className='mt-4'>
+                                <Alert variant="success" className={hiddenState ? "hidden" : ""}>
+                                    Thank you, your message has been sent!
+                                </Alert>
+                            </div>
                         </Form>
                     </Col>
 
